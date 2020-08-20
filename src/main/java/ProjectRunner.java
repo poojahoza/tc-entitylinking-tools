d115df838103fe5f8c6fb0e4fedf39ebabb31d51
poojahoza <oza.pooja.h@gmail.com>
2020-08-20 17:49:52 -0400

    DBpedia Entity Linker - coded ProjectRunner and DBpediaELIndexRunner

--- src/main/java/ProjectRunner.java
+++ src/main/java/ProjectRunner.java
@@ -1,7 +1,31 @@
 package main.java;
 
+import main.java.commandparser.CommandParser;
+import main.java.runner.*;
+
 /**
  * @author poojaoza
  **/
 public class ProjectRunner {
+
+    public static void main(String[] args)
+    {
+        /*
+          Parses the command line and creates the parser
+        */
+        CommandParser parser = new CommandParser(args);
+        ProgramRunner runner = null;
+        if(args.length < 1)
+        {
+            parser.getParser().usage();
+        }
+        else {
+            if (parser.getParser().getParsedCommand().equals("dbpedia")) {
+                runner = new DBpediaELIndexRunner(parser);
+                runner.run();
+            }else {
+                parser.getParser().usage();
+            }
+        }
+    }
 }
--- src/main/java/runner/DBpediaELIndexRunner.java
+++ src/main/java/runner/DBpediaELIndexRunner.java
@@ -3,6 +3,9 @@ package main.java.runner;
 import main.java.commandparser.CommandParser;
 import main.java.commandparser.RegisterCommands;
 import main.java.commandparser.ValidateCommands;
+import main.java.indexer.DBpediaIndexBuilder;
+
+import java.io.IOException;
 
 
 /**
@@ -22,6 +25,16 @@ public class DBpediaELIndexRunner implements ProgramRunner{
     @Override
     public void run(){
         validate.ValidateDBpediaEL();
+
+        String destFinalPath = dBpediaELParser.getDestpath()+"_paragraph";
+        DBpediaIndexBuilder dBpediaIndexBuilder;
+
+        try {
+            dBpediaIndexBuilder = new DBpediaIndexBuilder(dBpediaELParser.getIndexPath(), destFinalPath);
+            dBpediaIndexBuilder.createDBpediaIndex();
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
 
     }
 
